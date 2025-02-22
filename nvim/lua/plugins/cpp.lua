cc1d5ca888466866f16f9e34f4de7c698b821ae6
Alsu1990 <alexsukmanov1990@gmail.com>
2025-02-22 15:55:22 +0200

    nvim: update config

---
+++ nvim/lua/plugins/cpp.lua
@@ -0,0 +1,19 @@
+local M = {
+    {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+            -- NOTE: change clangd fallback to google
+            opts.servers.clangd.cmd = {
+                "clangd",
+                "--background-index",
+                "--clang-tidy",
+                "--header-insertion=iwyu",
+                "--completion-style=detailed",
+                "--function-arg-placeholders",
+                "--fallback-style=webkit",
+            }
+        end,
+    },
+}
+
+return M
---
+++ nvim/lua/plugins/verilog.lua
@@ -0,0 +1,54 @@
+local M = {
+    -- highlight
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "verilog",
+                "tcl",
+                "make",
+            })
+        end,
+    },
+
+    -- LSP
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "svlangserver",
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                svlangserver = {
+                    cmd = { "svlangserver" },
+                    filetypes = { "verilog", "systemverilog" },
+                    root_dir = require("lspconfig").util.root_pattern(
+                        ".svlangserver",
+                        ".git"
+                    ),
+                    settings = {
+                        systemverilog = {
+                            includeIndexing = {
+                                "*.{v,vh,sv,svh}",
+                                "**/*.{v,vh,sv,svh}",
+                                "/tools/Xilinx/Vivado/2023.2/data/verilog/src/unimacro/*.{v,vh,sv,svh}",
+                                "/tools/Xilinx/Vivado/2023.2/data/verilog/src/unisims/*.{v,vh,sv,svh}",
+                            },
+                            linter = "verilator",
+                            launchConfiguration = "verilator --sv --Wall --lint-only",
+                            formatCommand = "verible-verilog-format --indentation_spaces 4",
+                        },
+                    },
+                },
+            },
+        },
+        setup = {},
+    },
+}
+
+return M
